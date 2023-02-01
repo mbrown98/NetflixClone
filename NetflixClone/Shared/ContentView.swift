7e5b7a8fb2dd4d46fdaf945df380dc6a20687ffe
Matt Brown <matthewbrown0798@gmail.com>
2023-01-31 19:05:28 -0500

    first commit

---
+++ NetflixClone/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetflixClone
+//
+//  Created by Matt Brown on 1/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetflixClone/Shared/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Matt Brown on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
