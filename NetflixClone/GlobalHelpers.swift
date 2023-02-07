7fa47cd3130059a1ef482e63026e8dee09aa546e
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2023-02-07 18:49:09 -0500

    first part of large movie preview

--- NetflixClone/GlobalHelpers.swift
+++ NetflixClone/GlobalHelpers.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 // the ! bascially says that we know this url will work, and we are ok with force unwrapping this
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "Dark", thumbnailURL: URL(string: "https://picsum.photos/200/301")!)
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/302")!)
-let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/303")!)
-let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/304")!)
-let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/305")!)
-let exampleMovie6 = Movie(id: UUID().uuidString, name: "Afterlife", thumbnailURL: URL(string: "https://picsum.photos/200/306")!)
+let exampleMovie1 = Movie(id: UUID().uuidString, name: "Dark", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/305")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie6 = Movie(id: UUID().uuidString, name: "Afterlife", thumbnailURL: URL(string: "https://picsum.photos/200/306")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
 
 let exampleMovies: [Movie] = [exampleMovie1, exampleMovie2, exampleMovie3, exampleMovie4, exampleMovie5, exampleMovie6]
---
+++ NetflixClone/TopMoviePreview.swift
@@ -0,0 +1,65 @@
+//
+//  TopMoviePreview.swift
+//  NetflixClone
+//
+//  Created by Matt Brown on 2/7/23.
+//
+
+import SwiftUI
+import KingfisherSwiftUI
+
+struct TopMoviePreview: View {
+    
+    var movie: Movie
+    
+    func isCategoryLast(_ cat: String) -> Bool {
+        // get number of movies in category array
+        let catCount = movie.categories.count
+        
+        // we need to use if let, becuase firstIndex returns an optional, ex categories[100000]
+        // if we get an index enter the if block
+        if let index = movie.categories.firstIndex(of: cat) {
+            if index + 1 != catCount {
+                return false
+            }
+        }
+        return true
+    }
+    
+    var body: some View {
+        ZStack {
+            KFImage(movie.thumbnailURL)
+            // a pretty common approach for images
+                .resizable()
+                .scaledToFill()
+                .clipped()
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    ForEach(movie.categories, id: \.self) { category in
+                        HStack {
+                            Text(category)
+                            
+                            if !isCategoryLast(category) {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.blue)
+                                    .font(.system(size: 3))
+                            }
+                        }
+                    }
+                }
+                
+                Text("Row of Buttons")
+            }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview(movie: exampleMovie1)
+    }
+}
